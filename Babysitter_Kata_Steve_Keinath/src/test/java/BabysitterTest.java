0de4c96332206a34ed1bcea6ab91c995ec2e4d3c
Steve Keinath <skeinath@gmail.com>
2017-10-17 15:04:21 -0400

    refactor babysitterTest to change rates to variables

--- Babysitter_Kata_Steve_Keinath/src/test/java/BabysitterTest.java
+++ Babysitter_Kata_Steve_Keinath/src/test/java/BabysitterTest.java
@@ -3,45 +3,49 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class BabysitterTest {
-
+	
+	public static int startToBedtimePayRate = 12;
+	public static int bedtimeToMidnightRate = 8;
+	public static int midnightToLeaveRate = 16;
+	
 	@Test
 	public void CheckRateForTimeWorkedBetweenStartAndBedtimeOneHourShouldBe12() {
-		assertEquals(12,BabysitterCalculator.calculateWages(5,6,6));
+		assertEquals((1 * startToBedtimePayRate),BabysitterCalculator.calculateWages(5,6,6));
 	}
 	
 	@Test
 	public void CheckRateForTimeWorkedBetweenStartAndBedtimeTwoHourShouldBe24() {
-		assertEquals(24,BabysitterCalculator.calculateWages(5,7,7));
+		assertEquals((2 * startToBedtimePayRate),BabysitterCalculator.calculateWages(5,7,7));
 	}
 	
 	@Test
 	public void CheckRateForTimeWorkedBetweenBedtimeAndMidnightOneHourShouldBe8() {
-		assertEquals(8,BabysitterCalculator.calculateWages(11,11,12));
+		assertEquals((1 * bedtimeToMidnightRate),BabysitterCalculator.calculateWages(11,11,12));
 	}
 	
 	@Test
 	public void CheckRateForTimeWorkedBetweenBedtimeAndMidnightTwoHoursShouldBe16() {
-		assertEquals(16,BabysitterCalculator.calculateWages(10,10,12));
+		assertEquals((2 * bedtimeToMidnightRate),BabysitterCalculator.calculateWages(10,10,12));
 	}
 	
 	@Test
 	public void CheckRateForTimeWorkedBetweenMidnightAndLeaveOneHourShouldBe16() {
-		assertEquals(16,BabysitterCalculator.calculateWages(12,12,1));
+		assertEquals((1 * midnightToLeaveRate),BabysitterCalculator.calculateWages(12,12,1));
 	}
 	
 	@Test
 	public void CheckRateForTimeWorkedBetweenMidnightAndLeaveTwoHoursShouldBe32() {
-		assertEquals(32,BabysitterCalculator.calculateWages(12,12,2));
+		assertEquals((2 * midnightToLeaveRate),BabysitterCalculator.calculateWages(12,12,2));
 	}
 	
 	@Test
 	public void CheckRateCalcWhenStartIs6AndBedtimeis8AndLeaveIs12ShouldBe56() {
-		assertEquals(56,BabysitterCalculator.calculateWages(6,8,12));
+		assertEquals((2 * startToBedtimePayRate)+(4 * bedtimeToMidnightRate)+(0 * midnightToLeaveRate),BabysitterCalculator.calculateWages(6,8,12));
 	}
 	
 	@Test
 	public void CheckRateCalcWhenStartIs6AndBedtimeis8AndLeaveIs2ShouldBe88() {
-		assertEquals(88,BabysitterCalculator.calculateWages(6,8,2));
+		assertEquals((2 * startToBedtimePayRate)+(4 * bedtimeToMidnightRate)+(2 * midnightToLeaveRate),BabysitterCalculator.calculateWages(6,8,2));
 	}
 
 }
