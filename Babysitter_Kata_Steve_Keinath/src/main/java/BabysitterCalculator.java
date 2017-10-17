5eb3b84b81113e345125dcb623b34cf5c408e4a9
Steve Keinath <skeinath@gmail.com>
2017-10-17 15:00:01 -0400

    refactor to change rates to variables

--- Babysitter_Kata_Steve_Keinath/src/main/java/BabysitterCalculator.java
+++ Babysitter_Kata_Steve_Keinath/src/main/java/BabysitterCalculator.java
@@ -1,16 +1,20 @@
 
 public class BabysitterCalculator {
 
+	public static int startToBedtimePayRate = 12;
+	public static int bedtimeToMidnightRate = 8;
+	public static int midnightToLeaveRate = 16;
+	
 	public static Object calculateWages(int start, int bedtime, int leave) {
 		int calculatedRate = 0;
 		
-		calculatedRate += (bedtime - start) * 12;
+		calculatedRate += (bedtime - start) * startToBedtimePayRate;
 		
 		if (leave != bedtime && bedtime < 12)
-			calculatedRate += (12 - bedtime) * 8;
+			calculatedRate += (12 - bedtime) * bedtimeToMidnightRate;
 		
 		if (leave < 5)
-			calculatedRate += leave * 16;
+			calculatedRate += leave * midnightToLeaveRate;
 		
 		return calculatedRate;
 		
